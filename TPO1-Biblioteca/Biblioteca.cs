51c25477c12d204bee48dbf47b58af38ef514f5a
kopa2 <kopacek5@gmail.com>
2025-05-02 16:42:54 -0300

    Se le agregaron los atributos y metodos a las clases Biblioteca y Libro

--- TPO1-Biblioteca/Biblioteca.cs
+++ TPO1-Biblioteca/Biblioteca.cs
@@ -8,5 +8,67 @@ namespace TPO1_Biblioteca
 {
     internal class Biblioteca
     {
+        private List<Libro> libros;
+
+        public Biblioteca()
+        {
+            this.libros = new List<Libro>();
+        }
+
+        private Libro buscarLibro(string titulo)
+        {
+            Libro librobuscado = null;
+
+            int i = 0;
+
+            while (i < libros.Count && !libros[i].getTitulo().Equals(titulo))
+            {
+                i++;
+            }
+            if (i != libros.Count)
+            {
+                librobuscado = libros[i];
+            }
+            return librobuscado;
+        }
+
+        public bool agregarLibro(string titulo, string autor, string editorial)
+        {
+           bool seAgrego = false;
+            
+            if (buscarLibro(titulo) == null)
+            {
+               Libro nuevoLibro = new Libro(titulo, autor, editorial);
+               libros.Add(nuevoLibro);
+                seAgrego = true;
+                System.Console.WriteLine("Se agrego el nuevo libro");
+            }
+
+            return seAgrego;
+        }
+
+        public bool eliminarLibro(string titulo)
+        {
+            bool eliminar = false;
+            Libro libro;
+            libro = buscarLibro(titulo);
+
+            if (libro != null)
+            {
+                libros.Remove(libro);
+                eliminar = true;
+            }
+
+            return eliminar;
+        }
+
+        public void listarLibros()
+        {
+            foreach (Libro libro in libros)
+            {
+                Console.WriteLine(libro);
+            }
+
+        }
     }
 }
--- TPO1-Biblioteca/Libro.cs
+++ TPO1-Biblioteca/Libro.cs
@@ -3,10 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace TPO1_Biblioteca
 {
     internal class Libro
     {
+        private string titulo;
+        private string autor;
+        private string editorial;
+
+        public Libro(string titulo, string autor, string editorial)
+        {
+            this.titulo = titulo;
+            this.autor = autor;
+            this.editorial = editorial;
+        }
+
+        public string getTitulo()
+        {
+            return titulo;
+        }
+
+        public string getAutor()
+        {
+            return autor;
+        }
+
+        public string getEditorial()
+        {
+            return editorial;
+        }
+
+        public override string ToString()
+        {
+            string info = $"Titulo: {this.getTitulo()}, Autor: {this.getAutor}, Editorial: ${this.getEditorial}";
+
+            return info;
+        }
     }
 }
